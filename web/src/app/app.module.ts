203d7a124b652b03f02cbbfa8f9e5dcf17743439
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-08-24 10:29:06 +0800

    :sparkles: Set default route to /products

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+const routes: Routes = [
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./merchant/merchant.module').then((m) => m.MerchantModule),
+  },
+  { path: '**', redirectTo: 'products' },
+];
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- web/src/app/merchant/merchant.module.ts
+++ web/src/app/merchant/merchant.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MerchantComponent } from './merchant.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: MerchantComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   declarations: [MerchantComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class MerchantModule {}
